51c2958ba18dc66ee47100676a6720dea0938ea3
José Manuel Sánchez Simón <josess@Balidea.com>
2018-11-02 12:51:20 +0100

    Pruebas mejoras

--- InvoiceScanner/InvoiceScanner/MainPage.xaml.cs
+++ InvoiceScanner/InvoiceScanner/MainPage.xaml.cs
@@ -14,7 +14,7 @@ using XLabs.Platform.Device;
 namespace InvoiceScanner
 {
     public partial class MainPage : ContentPage
-    {        
+    {
         private readonly ITesseractApi _tesseractApi;
         private readonly IDevice _device;
         private Image _takenImage;
@@ -30,16 +30,25 @@ namespace InvoiceScanner
             }
         }
 
-
-
-
-
         public MainPage()
         {
             InitializeComponent();
             BindingContext = this;
             _tesseractApi = Resolver.Resolve<ITesseractApi>();
             _device = Resolver.Resolve<IDevice>();
+            initialize();
+        }
+
+        private async void initialize()
+        {
+            if (!_tesseractApi.Initialized)
+            {
+                await _tesseractApi.Init("spa");
+                _tesseractApi.SetPageSegmentationMode(PageSegmentationMode.SparseText);
+                string textPermited = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.;€:";
+                //abcdefghijklmnopqrstuvwxyz
+                _tesseractApi.SetWhitelist(textPermited);
+            }
         }
 
         async void TakePictureButton_Clicked(object sender, EventArgs e)
@@ -56,31 +65,41 @@ namespace InvoiceScanner
 
             var opts = new Plugin.Media.Abstractions.StoreCameraMediaOptions
             {
-                PhotoSize = PhotoSize.Medium,
-                SaveToAlbum = true,
-                CompressionQuality = 90,
-                SaveMetaData = false
-
-
+                //PhotoSize = PhotoSize.Full,
+                //SaveToAlbum = true,
+                //CompressionQuality = 90,
+                //SaveMetaData = false,
+                AllowCropping = true,                                
             };
 
             var file = await CrossMedia.Current.TakePhotoAsync(opts);
 
-            if (!_tesseractApi.Initialized)
-                await _tesseractApi.Init("spa");
+
+
 
             var photo = file?.GetStream();
             if (photo != null)
             {
-              
 
-                
+
+
                 var tessResult = await _tesseractApi.SetImage(photo);
                 if (tessResult)
                 {
-                    MyStringProperty = _tesseractApi.Text;
-                }                
+                    var textReconized = _tesseractApi.Text;
+                    var resultado = textReconized.Contains("Importe venta");
+                    var test = _tesseractApi.Results(PageIteratorLevel.Textline);
+                    foreach (var item in test)
+                    {
+                        if (item.Text.Contains("IMPORTE VENTA")) {
+                            MyStringProperty = item.Text;
+                        }
+
+                    }
+
+                }
             }
+
         }
 
     }
